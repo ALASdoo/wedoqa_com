dd977c81767656de34d8380e0b3f699c099e25be
Aleksandra Kosanovic <aleksandra@alasdoo.com>
2021-11-04 22:40:56 +0100

    Fix FF flickering on mobile device
    https://trello.com/c/fncZSrGg

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,4 +172,14 @@ jQuery(function ($) {
   }
 
 
+  /**
+   * Set height to Hero section based on the browser height
+   * fix flickering 
+   * https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+   */
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 });
